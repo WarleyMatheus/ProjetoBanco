e78cba18e20f5a2f5c8982e8514c4352c6e66322
WarleyMatheus <mwarley735@gmail.com>
2022-11-16 20:42:47 -0300

    intankavel Bostil

---
+++ src/com/mycompany/projetobanco2/mainClass.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetobanco2;
+
+
+public class mainClass {
+    public static void main(String[] args) {
+        interface_inicial tela = new interface_inicial();
+        tela.setVisible(true);
+    }
+    
+}
